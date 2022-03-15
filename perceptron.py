f2779d6fad818bae405e5131358dcf03f311489f
IsraEB <sr.enriquez@yahoo.com>
2022-03-15 13:25:36 -0600

    Listo

--- perceptron.py
+++ perceptron.py
@@ -22,8 +22,6 @@ class Neuron:
 				    "Error, el número de columnas de entrada no son homogeneas"
 				)
 
-			row.append(-1)
-
 		self.w = []
 
 		for i in range(row_length + 1):
@@ -35,10 +33,15 @@ class Neuron:
 			error_in_epoch = False
 
 			for n_row in range(len(self.x)):
+				self.x[n_row].append(-1)
+
 				print(self.x[n_row])
 				print("Old: ", self.__str__())
 
-				f_x_net = self.training_predict(self.x[n_row])
+				self.x[n_row].pop()
+				f_x_net = self.predict(self.x[n_row])
+				self.x[n_row].append(-1)
+
 				print("f(x_net): ", f_x_net)
 				print("S: ", self.y[n_row])
 
@@ -54,22 +57,23 @@ class Neuron:
 				if n_error != 0:
 					error_in_epoch = True
 
+				self.x[n_row].pop()
+
 			if error_in_epoch == False:
-				print("Se han corregido exitosamente todos los pesos")
-				break
+				print("Se han corregido exitosamente todos los pesos\n")
+				return
+
+			self.print_dataset_predictions()
+
+		print(
+		    "Se ha alcanzado el número máximo de épocas y no se han encontrado unos pesos correctos"
+		)
 
 	def activation_function(self, x):
 		if (x <= 0):
 			return -1
 		else:
 			return 1
-
-	def training_predict(self, x):
-		x_net = 0
-		for n_column in range(len(x)):
-			x_net = x_net + (x[n_column] * self.w[n_column])
-
-		return self.activation_function(x_net)
 
 	def predict(self, x):
 		x.append(-1)
@@ -90,18 +94,21 @@ class Neuron:
 
 		return string
 
+	def print_dataset_predictions(self):
+		for n_row in range(len(self.x)):
+			f_x_net = self.predict(self.x[n_row])
+			print(self.x[n_row], ": ", f_x_net)
+
+		print()
+
 
 import csv
 
-with open('test.csv') as csvfile:
-	rows = csv.reader(csvfile)
-	res = list(zip(*rows))
-
-	res = [list(filter(None.__ne__, l)) for l in res]
+with open('test.csv', newline='') as f:
+	reader = csv.reader(f)
+	res = list(reader)
 
 	res = [list(map(int, i)) for i in res]
-
-	print("res:", res)
 
 	x = [row[:-1] for row in res]
 	y = [row[-1] for row in res]
@@ -112,11 +119,4 @@ with open('test.csv') as csvfile:
 
 	neuron.train(10000)
 
-	test = [-1, -1]
-	print(test, neuron.predict(test))
-	test = [-1, 1]
-	print(test, neuron.predict(test))
-	test = [1, -1]
-	print(test, neuron.predict(test))
-	test = [1, 1]
-	print(test, neuron.predict(test))
+	neuron.print_dataset_predictions()
